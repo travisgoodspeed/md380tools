09367505eed0f70ec01838e5d21c50d1ce74f497
Travis Goodspeed <travis@radiantmachines.com>
2015-11-19 04:48:33 -0500

    Found the Welcome screen.  Will patch it next.

--- patches/2.032/patch.py
+++ patches/2.032/patch.py
@@ -71,14 +71,26 @@ if __name__ == '__main__':
 
     patcher.nopout(0x0803ef10,0xd11f);  #Matches private calls too.
     patcher.export("prom-private.img");
-    
 
-    #This corrupts a bit of the startup image, which begins at 0x08094610.
-    patcher.nopout(0x08094610);
-    patcher.nopout(0x08094612);
-    patcher.nopout(0x08094614);
-    patcher.nopout(0x08094616);
-    patcher.nopout(0x08094618);
+    #Everything after here is experimental.
+    #Everything after here is experimental.
+    #Everything after here is experimental.
+    
+    
+    #This corrupts the first row of the startup image, which begins at 0x08094610.
+    #patcher.ffrange(0x08094610, 0x08094610+0x14);
+
+    sprite="";
+    for i in range(0,768):
+        b=patcher.getbyte(0x08094610+i);
+        if i%0x14==0:
+            sprite=sprite+"\n";
+        for j in range(0,8):
+            c=' ';
+            if b&(128>>j)>0:
+                c='X';
+            sprite=sprite+c;
+    print sprite;
     
     #This cuts out the Chinese font, freeing ~200k for code patches.
     patcher.ffrange(0x809c714,0x80d0f80);
