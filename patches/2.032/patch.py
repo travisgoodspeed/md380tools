5ea4ad642fe78643ded1c507030498e91cd78485
Travis Goodspeed <travis@radiantmachines.com>
2015-11-19 06:25:03 -0500

    Put the patched logo in all firmware images.

--- patches/2.032/patch.py
+++ patches/2.032/patch.py
@@ -7,6 +7,13 @@ from Patcher import Patcher
 if __name__ == '__main__':
     print "Creating patches from unwrapped.img.";
     patcher=Patcher("unwrapped.img");
+
+    #All patched images should be indicated by a patched welcome screen.
+    fhello=open("welcome.txt","rb");
+    hello=fhello.read();
+    patcher.str2sprite(0x08094610,hello);
+    print patcher.sprite2str(0x08094610,0x14,760);
+    
     
     patcher.nopout(0x0803ee36,0xd1ef);  #Matches first group for public calls.
     patcher.export("prom-public.img");
@@ -17,15 +24,6 @@ if __name__ == '__main__':
     #Everything after here is experimental.
     #Everything after here is experimental.
     #Everything after here is experimental.
-    
-    
-    #This corrupts the first row of the startup image, which begins at 0x08094610.
-    #patcher.ffrange(0x08094610, 0x08094610+0x14);
-    fhello=open("welcome.txt","rb");
-    hello=fhello.read();
-    patcher.str2sprite(0x08094610,hello);
-    print patcher.sprite2str(0x08094610,0x14,760);
-    
     
     #This cuts out the Chinese font, freeing ~200k for code patches.
     patcher.ffrange(0x809c714,0x80d0f80);
