8c0bd43429a12033ede1499408df7072a731c02d
Travis Goodspeed <travis@radiantmachines.com>
2016-02-07 22:07:26 -0500

    Applet linking script now uses RAM that isn't clobbered by the radio application.

--- apps/2.032/blinky/src/main.c
+++ apps/2.032/blinky/src/main.c
@@ -23,6 +23,9 @@
 #include "stm32f4_discovery.h"
 #include "stm32f4xx_conf.h" // again, added because ST didn't put it here ?
 
+
+#include <stdio.h>
+
 /** @addtogroup STM32F4_Discovery_Peripheral_Examples
   * @{
   */
@@ -117,7 +120,84 @@ static void red_led(int on) {
   }
 }
 
+//Must be const, because globals will be wacked.
+//const wchar_t mfgstr[]=L"Travis KK4VCZ";
+const char mfgstr[]="Travis Goodspeed KK4VCZ"; //"\x1c\x03T\0r\0a\0v\0i\0s\0 \0K\0K\x004\0V\0C\0Z\0";
 
+/* This copies a character string into a USB Descriptor string, which
+   is a UTF16 little-endian string preceeded by a one-byte length and
+   a byte of 0x03. */
+const char *loadusbstr(char *usbstring,
+		       char *newstring,
+		       long *len){
+  int i=0;
+  *len=2;
+  
+  usbstring[1]=3;//Type
+
+  while(newstring[i]){
+    usbstring[2+2*i]=newstring[i];//The character.
+    usbstring[3+2*i]=0;           //The null byte.
+    i++;
+  }
+  *len=2*i+2;
+  usbstring[0]=*len;
+  return usbstring;
+}
+
+
+//const 
+char hexes[]="0123456789abcdef"; //Needs to be const for problems.
+
+/* This writes a 32-bit hexadecimal value into a human-readable
+   string.  We do it manually to avoid heap operations. */
+void strhex(char *string, long value){
+  char b;
+  for(int i=0;i<4;i++){
+    b=value>>(24-i*8);
+    string[2*i]=hexes[(b>>4)&0xF];
+    string[2*i+1]=hexes[b&0xF];
+  }
+}
+
+const char *getmfgstr(int speed, long *len){
+  //This will be turned off by another thread,
+  //but when we call it often the light becomes visible.
+  green_led(1);
+  
+  //Only buffer that we know is free.
+  //This is used by the MD380 firmware.
+  //long *adr=(long*) 0x20004834; //Hope this adr is free.
+  long *adr=(long*) 0x2001fffc; //This address is known to be free.
+  long *val;
+  char *usbstring=(char*) 0x2001c080;
+  char buffer[]="@________ : ________";
+  
+  if(*adr&0x3 || *adr<0x10000000 || *adr>0x10030000)
+    *adr=0x10000000;
+  
+  val=(long*) *adr;
+  
+  //Can we overwrite it?
+  //*val=0xcafebabe;
+  
+  strhex(buffer+1,(long) *adr);
+  strhex(buffer+12,(long) *val);
+
+
+  adr[0]+=4;
+  
+  
+  //Return the string as our value.
+  return loadusbstr(usbstring,buffer,len);
+}
+
+void wipe_mem(){
+  long *start=(long*) 0x10000000;
+  long *end=(long*)   0x10010000;
+  while(start<end)
+    *start++=0xdeadbeef;
+}
 
 /**
   * @brief  Main program
@@ -133,6 +213,8 @@ int main(void)
         system_stm32f4xx.c file
      */
   
+  //Initialize RAM to zero, to figure out what's free later.
+  //wipe_mem();
   
   led_setup();
   
@@ -160,6 +242,10 @@ int main(void)
   
   //Done with the blinking, so start the radio application.
   abort_to_mfgr_app();
+
+  //These never get run, but we call them anyways to keep them in the
+  //binary.
+  getmfgstr(0,&main);
 }
 
 
