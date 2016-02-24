ce47c270cdbfe4dd1837fe7b17446f4ee5744af0
Travis Goodspeed <travis@radiantmachines.com>
2016-02-24 17:48:01 -0500

    Another color-code check, nopping of which does nothing.

--- patches/2.032/patch.py
+++ patches/2.032/patch.py
@@ -31,6 +31,8 @@ if __name__ == '__main__':
 #     patcher.nopout(0x0803ea64,0x80fd);
 #     patcher.nopout(0x0803e994,0xf040);  #Late Entry CC check.
 #     patcher.nopout(0x0803e996,0x8164);
+#     patcher.nopout(0x0803fd98);  #dmr_dll_parser CC check.
+#     patcher.nopout(0x0803fd9a);
 #     patcher.sethword(0x0803fd8e,0xe02d, #Check in dmr_dll_parser().
 #                      0xd02d);
 #     patcher.nopout(0x0803eafe,0xf100); #Disable CRC check, in case CC is included.
