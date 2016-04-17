f423b81a365dea1f799e8d8ce477407795bf0596
Alex Eickhoff <alex@artbanause.de>
2016-04-17 20:20:59 +0200

    +Debug Menu (first small steps) :)

--- applet/src/menu.c
+++ applet/src/menu.c
@@ -79,6 +79,18 @@ e0020000 Selected Page Index
 }
 
 
+
+
+void F_249_Create_MenuEntry_hook(int a, void * b , void * c, void  * d, int e, int f ,int g) {
+  printf("0x%x Text: 0x%x GreenKey 0x%x RedKey 0x%x 0x%x 0x%x 0x%x\n", a,b,c,d,e,f,g);
+  printf("b: ");
+  printhex2(b,14);
+  printf("\n");
+  printf("%x %\n", * (unsigned int*) 0x20019df0);
+  F_249_Create_MenuEntry(a,b,c,d,e,f,g);
+}
+
+
 static void do_jump(uint32_t entrypoint) {
   asm volatile(
                "bx     %0      \n"
@@ -90,17 +102,56 @@ static void do_jump(uint32_t entrypoint) {
 
 void Create_Menu_Utilies_hook(void) {
 
-  uint8_t * program_radio_unprohibited = (uint8_t *) (0x2001d030 + 4);
+    uint8_t * program_radio_unprohibited = (uint8_t *) (0x2001d030 + 4);
+    uint32_t menu_enabled;   
+    uint32_t menu_unknown_0; 
+    uint32_t menu_unknown_1; 
+    uint32_t menu_entry_nr;  
+    void *menu_green_key, *menu_red_key,*menu_text;
+    static wchar_t wide[40];
+    char my_entry[]="Debug";
+    int i;  
+                                                     
+  
   // 0x08012740      dff8f80a       ldr.w r0, [pc, 0xaf8]       ; [0x801323c:4]=0x2001d030
   // 0x08012744      0079           ldrb r0, [r0, 4]   
   // 0x08012746      c0f38000       ubfx r0, r0, 2, 1 
   // 0x0801274a      c007           lsls r0, r0, 0x1f
   // 0x0801274c      02d4           bmi 0x8012754               ;[2] ; Is Menu ProgramRadio allowedly  
   if ( (* program_radio_unprohibited & 0x4) == 0x4 ) {
-    printf("program_radio_unprohibited ");  
+    printf("program_radio_unprohibited\n");  
   } else {
-    printf("program_radio_prohibited ");
+    printf("program_radio_prohibited\n");
   }
+  // 0x0801274e      54f00104       orrs r4, r4, 1                                                                 
+  // 0x08012752      00e0           b 0x8012756                 ;[3]                                               
+  // 0x08012754      0024           movs r4, 0                                                                     
+  // 0x08012756      e4b2           uxtb r4, r4                                                                    
+  // 0x08012758      a4b2           uxth r4, r4       
+  // 0x0801275a      0294           str r4, [sp, 8]     
+  menu_enabled=1;
+  menu_unknown_0=0;
+  menu_unknown_1=0x8a;
+  menu_green_key=0x80127d1;
+  menu_red_key=0x800f453; //back
+  menu_text=0x080d175c;
+  menu_entry_nr=8;
+  F_249_Create_MenuEntry_hook(menu_entry_nr, menu_text , menu_green_key , menu_red_key, menu_unknown_1, menu_unknown_0 , menu_enabled);
+
+  for(i=0;i<strlen(my_entry);i++)
+    wide[i]=my_entry[i];
+
+  wide[i]='\0';   
+  menu_enabled=1;
+  menu_unknown_0=0;
+  menu_unknown_1=0x8a;
+  menu_green_key=0x80127d1;
+  menu_red_key=0x800f453; //back
+
+  menu_entry_nr=9;
+  F_249_Create_MenuEntry_hook(menu_entry_nr, wide , menu_green_key , menu_red_key, menu_unknown_1, menu_unknown_0 , menu_enabled);
+  
+  
 }  
   
 
@@ -108,13 +159,3 @@ void Create_Menu_Utilies_hook_end(void) {
   do_jump(0x08012786+1);
 }
 
-
-
-void F_249_Create_MenuEntry_hook(int a, void * b , void * c, void  * d, int e, int f ,int g) {
-  printf("0x%x Text: 0x%x GreenKey 0x%x RedKey 0x%x 0x%x 0x%x 0x%x\n", a,b,c,d,e,f,g);
-  printf("b: ");
-  printhex2(b,10);
-  printf("\n");
-//  printf("%x %\n", * (unsigned int*) 0x20019df0);
-  F_249_Create_MenuEntry(a,b,c,d,e,f,g);
-}
