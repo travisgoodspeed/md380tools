48265e22a2bab3d2f6a49f7c95acfdcd3f93576a
Travis Goodspeed <travis@radiantmachines.com>
2016-04-24 16:32:51 -0400

    Better AMBE printing comments.

--- applet/config.h
+++ applet/config.h
@@ -5,14 +5,23 @@
 */
 
 
-/* Uncomment these to enable printing raw frames, either before or
-   after error correction is applied. */
-//#define AMBECORRECTEDPRINT
-//#define AMBEUNCORRECTEDPRINT
+/* Uncomment this to print AMBE frames for decoding with DSD.  You
+   probable want this instead of AMBECORRECTEDPRINT or
+   AMBEUNCORRECTEDPRINT below.
+*/
 //#define AMBEPRINT
 
 /* This one prints all incoming audio as WAV.  Very verbose, but it
    allows for nice clean recordings of DMR audio.
 */
 //#define AMBEWAVPRINT
+
+
+
+
+/* Uncomment these to enable printing raw frames, either before or
+   after error correction is applied. */
+//#define AMBECORRECTEDPRINT
+//#define AMBEUNCORRECTEDPRINT
+
 
