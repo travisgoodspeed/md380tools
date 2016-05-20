5d3d018516b436665ee994358818b9d70bf69b4f
Alex Eickhoff <alex@artbanause.de>
2016-05-21 00:58:06 +0200

    micbargraph has now an own menuentry
    Conflicts:
    	applet/src/menu.c

--- applet/src/menu.c
+++ applet/src/menu.c
@@ -20,20 +20,21 @@
 #include "spiflash.h"
 #include "addl_config.h"
 
-static wchar_t wt_addl_func[]         = L"MD380Tools";
-static wchar_t wt_datef[]             = L"Date format";
-static wchar_t wt_debug[]             = L"Debug";
-static wchar_t wt_disable[]           = L"Disable";
-static wchar_t wt_enable[]            = L"Enable";
-static wchar_t wt_rbeep[]             = L"M. RogerBeep";
-static wchar_t wt_userscsv[]          = L"UsersCSV";
-static wchar_t wt_datef_original[]    = L"Original";
-static wchar_t wt_datef_germany[]     = L"German";
-static wchar_t wt_promtg[]            = L"Promiscuous";
-static wchar_t wt_edit[]              = L"Edit";
-static wchar_t wt_edit_dmr_id[]       = L"Edit DMR-ID";
-static wchar_t wt_no_w25q128[]        = L"No W25Q128";
-static wchar_t wt_experimental[]      = L"Experimental";
+const static wchar_t wt_addl_func[]         = L"MD380Tools";
+const static wchar_t wt_datef[]             = L"Date format";
+const static wchar_t wt_debug[]             = L"Debug";
+const static wchar_t wt_disable[]           = L"Disable";
+const static wchar_t wt_enable[]            = L"Enable";
+const static wchar_t wt_rbeep[]             = L"M. RogerBeep";
+const static wchar_t wt_userscsv[]          = L"UsersCSV";
+const static wchar_t wt_datef_original[]    = L"Original";
+const static wchar_t wt_datef_germany[]     = L"German";
+const static wchar_t wt_promtg[]            = L"Promiscuous";
+const static wchar_t wt_edit[]              = L"Edit";
+const static wchar_t wt_edit_dmr_id[]       = L"Edit DMR-ID";
+const static wchar_t wt_no_w25q128[]        = L"No W25Q128";
+const static wchar_t wt_experimental[]      = L"Experimental";
+const static wchar_t wt_micbargraph[]       = L"Mic bargraph";
 
 struct MENU {
   void    *menu_titel;
@@ -159,6 +160,40 @@ void create_menu_entry_promtg_disable_screen(void) {
   create_menu_entry_hook( *md380_menu_id, wt_disable, md380_menu_entry_back+1, md380_menu_entry_back+1, 6, 2 , 1);
   spiflash_write_with_type_check("0", spi_flash_addl_config_start + offset_promtg, 1);
   global_addl_config.promtg=0;
+}
+
+void create_menu_entry_micbargraph_enable_screen(void) {
+  struct MENU *menu_mem;
+
+  menu_mem = (md380_menu_memory + ((*md380_menu_depth) * sizeof(struct MENU))) + sizeof(struct MENU);
+  menu_mem->menu_titel = wt_micbargraph;
+
+  menu_mem->unknownp = 0x14 * *md380_menu_id + md380_menu_mem_base;
+
+  menu_mem->numberof_menu_entries=1;
+  menu_mem->unknown_00 = 0;
+  menu_mem->unknown_01 = 0;
+
+  create_menu_entry_hook( *md380_menu_id, wt_enable, md380_menu_entry_back+1, md380_menu_entry_back+1, 6, 2 , 1);
+  spiflash_write_with_type_check("1", spi_flash_addl_config_start + offset_micbargraph, 1);
+  global_addl_config.micbargraph=1;
+}
+
+void create_menu_entry_micbargraph_disable_screen(void) {
+  struct MENU *menu_mem;
+
+  menu_mem = (md380_menu_memory + ((*md380_menu_depth) * sizeof(struct MENU))) + sizeof(struct MENU);
+  menu_mem->menu_titel = wt_micbargraph;
+
+  menu_mem->unknownp = 0x14 * *md380_menu_id + md380_menu_mem_base;
+
+  menu_mem->numberof_menu_entries=1;
+  menu_mem->unknown_00 = 0;
+  menu_mem->unknown_01 = 0;
+
+  create_menu_entry_hook( *md380_menu_id, wt_disable, md380_menu_entry_back+1, md380_menu_entry_back+1, 6, 2 , 1);
+  spiflash_write_with_type_check("0", spi_flash_addl_config_start + offset_micbargraph, 1);
+  global_addl_config.micbargraph=0;
 }
 
 void create_menu_entry_rbeep_enable_screen(void) {
@@ -356,6 +391,37 @@ void create_menu_entry_promtg_screen(void) {
 
   create_menu_entry_hook( *md380_menu_id,     wt_enable,  create_menu_entry_promtg_enable_screen+1, md380_menu_entry_back+1,  0x8b, 0 , 1);
   create_menu_entry_hook( *md380_menu_id + 1, wt_disable, create_menu_entry_promtg_disable_screen+1, md380_menu_entry_back+1, 0x8b, 0 , 1);
+
+  for(i=0;i<2;i++) { // not yet known ;)
+    uint8_t *p;
+    p = md380_menu_mem_base + ( *md380_menu_id + i ) * 0x14;
+    p[0x10] = 0;
+  }
+}
+
+void create_menu_entry_micbargraph_screen(void) {
+  int i;
+  struct MENU *menu_mem;
+  int8_t buf[1];
+
+  md380_spiflash_read(buf, spi_flash_addl_config_start + offset_micbargraph, 1);
+
+  menu_mem = (md380_menu_memory + ((*md380_menu_depth) * sizeof(struct MENU))) + sizeof(struct MENU);
+  menu_mem->menu_titel = wt_micbargraph;
+
+  menu_mem->unknownp = 0x14 * *md380_menu_id + md380_menu_mem_base;
+  menu_mem->numberof_menu_entries=2;
+  menu_mem->unknown_00 = 0;
+  menu_mem->unknown_01 = 0;
+
+  if (buf[0] == '1') {
+    *md380_menu_entry_selected = 0;
+  } else {
+    *md380_menu_entry_selected = 1;
+  }
+
+  create_menu_entry_hook( *md380_menu_id,     wt_enable,  create_menu_entry_micbargraph_enable_screen+1, md380_menu_entry_back+1,  0x8b, 0 , 1);
+  create_menu_entry_hook( *md380_menu_id + 1, wt_disable, create_menu_entry_micbargraph_disable_screen+1, md380_menu_entry_back+1, 0x8b, 0 , 1);
 
   for(i=0;i<2;i++) { // not yet known ;)
     uint8_t *p;
@@ -695,7 +761,7 @@ void create_menu_entry_addl_functions_screen(void) {
 
   menu_mem->unknownp = 0x14 * *md380_menu_id + md380_menu_mem_base;
 
-  menu_mem->numberof_menu_entries=8;
+  menu_mem->numberof_menu_entries=9;
   menu_mem->unknown_00 = 0;
   menu_mem->unknown_01 = 0;
 
@@ -713,10 +779,12 @@ void create_menu_entry_addl_functions_screen(void) {
                           md380_menu_entry_back+1, 0x8a, 0 , 0);  // disable this menu entry - no function jet 
   create_menu_entry_hook( *md380_menu_id + 6, wt_edit_dmr_id, create_menu_entry_edit_dmr_id_screen+1,
                           md380_menu_entry_back+1, 0x8a, 0 , 1);
-  create_menu_entry_hook( *md380_menu_id + 7, wt_experimental, create_menu_entry_experimental_screen+1,
+  create_menu_entry_hook( *md380_menu_id + 7, wt_micbargraph, create_menu_entry_micbargraph_screen+1,
+                          md380_menu_entry_back+1, 0x98, 0 , 1);
+  create_menu_entry_hook( *md380_menu_id + 8, wt_experimental, create_menu_entry_experimental_screen+1,
                           md380_menu_entry_back+1, 0x8a, 0 , 1);
 
- for(i=0;i<8;i++) {  // not yet known ;)
+ for(i=0;i<9;i++) {  // not yet known ;)
    uint8_t *p;
    p = md380_menu_mem_base + ( *md380_menu_id + i ) * 0x14;
    p[0x10] = 2;
