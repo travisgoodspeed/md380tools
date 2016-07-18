23fc5a3e9bf8df969f86ebe1f2b1ca583c6fecd9
Alex Eickhoff <alex@artbanause.de>
2016-07-18 19:16:38 +0200

    remove warning

--- applet/src/display.c
+++ applet/src/display.c
@@ -10,7 +10,7 @@
 // see issue #178
 // workaround flipped display phenomenon
 
-display_init_hook_1(void) {           // from 0x8033586 @ D003.020
+void display_init_hook_1(void) {      // from 0x8033586 @ D003.020
   if ( md380_radio_config[0x1d] & 1)  // offset 0x1d from
                                       // 0x08033586 @ D003.020
    Write_Data_2display(0x48);         // md390, no gps, config
@@ -18,7 +18,7 @@ display_init_hook_1(void) {           // from 0x8033586 @ D003.020
    Write_Data_2display(0x8);          // orginal MD380
 }
 
-display_init_hook_2(void) {
+void display_init_hook_2(void) {
   if ( md380_radio_config[0x1d] & 1)
     Write_Data_2display(0x4f);
   else
