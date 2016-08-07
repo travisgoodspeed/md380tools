6638ecf55a3c41195dd776d0f530fb2e3fce5f7d
AndyTaylorTweet <andy@always-online.co.uk>
2016-08-07 19:01:22 +0100

    Create patch.py

---
+++ patches/3.020/patch.py
@@ -0,0 +1,17 @@
+file
+#!/usr/bin/env python2
+# Vocoder Patch for MD380 Firmware
+# Applies to version D013.020
+
+from Patcher import Patcher
+
+#Match all public calls.
+monitormode=False
+#Match all private calls.
+monitormodeprivate=False
+
+if __name__ == '__main__':
+    print "Creating patches from unwrapped.img."
+    patcher=Patcher("unwrapped.img")
+    
+    patcher.export("patched.img")
