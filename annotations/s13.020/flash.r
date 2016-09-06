4fdd6756f762a090f063f3eddd2ebb7a47f98dc4
Alexander Eickhoff <alex@artbanause.de>
2016-09-06 23:01:39 +0200

    .. Bootstrapping S013.020 all symbols now, usb hooks active

--- annotations/s13.020/flash.r
+++ annotations/s13.020/flash.r
@@ -1,71 +1,93 @@
 af+ 0x800c188 1446 md380_create_main_meny_entry
-af+ 0x800c72e 86 md380_create_menu_entry
-af+ 0x800def7 36 gfx_drawtext
-af+ 0x800fc84 18 md380_menu_entry_back
-af+ 0x801580c 220 Create_Menu_Entry_RX_QRG_1
-af+ 0x8015966 102 Create_Menu_Entry_RX_QRG_3
-af+ 0x80159ec 628 Create_Menu_Entry_RX_QRG_4
+af+ 0x800c72e 86 md380_create_menu_entry **
+af+ 0x800def6 36 gfx_drawtext **
+af+ 0x800fc84 18 md380_menu_entry_back **
+af+ 0x801353c 488 Create_Menu_Utilies **
+af+ 0x80137b0 932 Create_Menu_Entry_ProgramRadio **
+af+ 0x8015c60 108 Create_Menu_Entry_RX_QRG_shown **
+CCa 0x8015cb2 Create_Menu_Entry_RX_QRG_1 **
+af+ 0x8015cd4 226 Create_Menu_Entry_RX_QRG_1
+CCa 0x8015d9c Create_Menu_Entry_RX_QRG_2
+CCa 0x8015da0 Create_Menu_Entry_RX_QRG_3
+af+ 0x8015db8 126 Create_Menu_Entry_RX_QRG_2
+af+ 0x8015e44 116 Create_Menu_Entry_RX_QRG_3
+CCa 0x8015e94 Create_Menu_Entry_RX_QRG_4
+af+ 0x8015ec0 584  Create_Menu_Entry_RX_QRG_4
 af+ 0x8016b7e 58 F_4158
-af+ 0x8018c5c 236  md380_itow
-af+ 0x801d49c 8 gfx_set_bg_color
-af+ 0x801d4a4 8 gfx_set_fg_color
-af+ 0x801d9c0 30 gfx_blockfill
-af+ 0x801de3c 18 gfx_chars_to_display
-af+ 0x801de3c 18 gfx_drawtext2
-af+ 0x8021b50 16 gfx_select_font
+af+ 0x8018c5c 236  md380_itow **
+af+ 0x801b172 26 md380_menu_numerical_input
+af+ 0x801d49c 8 gfx_set_bg_color **
+af+ 0x801d4a4 8 gfx_set_fg_color **
+af+ 0x801d9c0 30 gfx_blockfill **
+af+ 0x801de3c 18 gfx_chars_to_display **
+af+ 0x801de3c 18 gfx_drawtext2 **
+af+ 0x8021b50 16 gfx_select_font **
 af+ 0x8022846 324 aes_startup_check
 af+ 0x802299c 18 Get_Welcome_Line1_from_spi_flash
 af+ 0x80229ae 18 Get_Welcome_Line2_from_spi_flash
-af+ 0x8023ada 86 gfx_drawbmp
+af+ 0x8023ada 86 gfx_drawbmp **
 af+ 0x80241c0 394 Edit_Message_Menu_Entry
 af+ 0x8025dc0 888 F_4315
-af+ 0x802b6d2 80 md380_RTC_GetTime
-af+ 0x802b7e8 76 md380_RTC_GetDate
+af+ 0x802b6d2 80 md380_RTC_GetTime **
+af+ 0x802b7e8 76 md380_RTC_GetDate **
 af+ 0x802fe30 4056 Beep_Process
+af+ 0x80314d8 258 md380_OSMboxPend **
 af+ 0x80315f0 86 md380_OSMboxPost
-af+ 0x80316b2 52 md380_spiflash_sektor_erase4k
-af+ 0x80316e6 52 md380_spiflash_block_erase64k
-af+ 0x80318b2 70 md380_spiflash_read
-af+ 0x80318f8 58 md380_spi_sendrecv
-af+ 0x8031944 34 md380_spiflash_wait
-af+ 0x8031966 28 md380_spiflash_enable
-af+ 0x8031982 24 md380_spiflash_disable
-af+ 0x803199a 704 md380_spiflash_write
-af+ 0x8031cec 78 md380_spiflash_security_registers_read
-af+ 0x80349cc 104 OSTimeDly
+af+ 0x80316b2 52 md380_spiflash_sektor_erase4k **
+af+ 0x80316e6 52 md380_spiflash_block_erase64k **
+af+ 0x80318b2 70 md380_spiflash_read **  
+af+ 0x80318f8 58 md380_spi_sendrecv **
+af+ 0x8031944 34 md380_spiflash_wait **
+af+ 0x8031966 28 md380_spiflash_enable **
+af+ 0x8031982 24 md380_spiflash_disable **
+af+ 0x803199a 704 md380_spiflash_write **
+af+ 0x8031cec 78 md380_spiflash_security_registers_read **
+af+ 0x80349cc 104 OSTimeDly **
 af+ 0x8034a48 146 licence_check
 af+ 0x8037750 136 aes_cipher
 af+ 0x80377d8 236 aes_loadkey
-af+ 0x803beb2 144 main_menu
-af+ 0x8040220 76 OSSemCreate
-af+ 0x804026c 218 OSSemPend
-af+ 0x804035c 92 OSSemPost
-af+ 0x8040a9c 38 c5000_spi0_writereg
-af+ 0x8040ae8 42 c5000_spi0_readreg
+af+ 0x803beb2 144 main_menu **
+af+ 0x8040220 76 OSSemCreate **
+af+ 0x804026c 218 OSSemPend **
+af+ 0x804035c 92 OSSemPost **
+af+ 0x8040a9c 38 c5000_spi0_writereg **
+af+ 0x8040ae8 42 c5000_spi0_readreg **
 af+ 0x804151a 740 dmr_call_start
 af+ 0x80417fe 92 dmr_before_squelch
 af+ 0x80418f8 1540 dmr_sms_arrive
 af+ 0x8041f48 864 dmr_call_end
-af+ 0x8044924 8 OS_ENTER_CRITICAL
-af+ 0x804492c 6 OS_EXIT_CRITICAL
-af+ 0x8046f80 60 Start_multiple_tasks
+af+ 0x8044924 8 OS_ENTER_CRITICAL **
+af+ 0x804492c 6 OS_EXIT_CRITICAL **
+af+ 0x8046f80 60 Start_multiple_tasks **
 af+ 0x804990a 186 alpu_transfer_init
 af+ 0x8049c3c 22 PWR_GetFlagStatus
 af+ 0x8049dbe 156 alpu_check_licence
 af+ 0x804ac40 978 Start_2_more_tasks__init_vocoder_tasks
 af+ 0x804c060 202 ambe_unpack
 af+ 0x804eb9c 430 dmr_handle_data
-af+ 0x804f3ac 204 OSTaskCreateExt
-af+ 0x804f478 90 OSTaskNameSet
+af+ 0x804f3ac 204 OSTaskCreateExt **
+af+ 0x804f478 90 OSTaskNameSet **
 af+ 0x8054018 124 ambe_encode_thing
 af+ 0x80544c0 140 ambe_decode_wav
 af+ 0x8055f40 68 usb_setcallbacks
-af+ 0x805a942 40 usb_send_packet
-af+ 0x808f970 190 usb_do_setup
-af+ 0x808fa2e 1444 usb_dnld_handle
-af+ 0x8090148 3036 usb_upld_handle
-af+ 0x80911b0 80 usb_dfu_write
-af+ 0x8091200 54 usb_dfu_read
+af+ 0x805a942 40 usb_send_packet **
+af+ 0x808f970 190 usb_do_setup **
+af+ 0x808fa2e 1444 usb_dnld_handle **
+af+ 0x8090148 3036 usb_upld_handle **
+af+ 0x80911b0 80 usb_dfu_write **
+af+ 0x8091200 54 usb_dfu_read **
 af+ 0x80974ee 34 usb_serialnumber
 CCa 0x80d0df0 md380_wt_programradio
 CCa 0x80f93d0 welcomebmp
+af+ 0x0801ae7e 246 Create_Menu_Entry_LEDIndicator **
+af+ 0x0801ff90 5832 md380_f_4225 **
+af+ 0x0802e3f4 1902 md380_f_4137 **
+af+ 0x0802cb18 2462 md380_f_4520 **
+af+ 0x0804f990 172 md380_f_4098 **
+af+ 0x0804faa6 298 md380_f_4102 **
+af+ 0x08030f2c 16 md380_f_294 **
+af+ 0x08046fbc 314 Start
+
+
+
+
--- applet/merge_s13.020.py
+++ applet/merge_s13.020.py
@@ -153,13 +153,13 @@ if __name__== '__main__':
     #Open the applet symbols
     sapplet=Symbols("%s.sym"%sys.argv[2]);
 
-    merger.hookstub(   #0x0809661e,    #USB manufacturer string handler function.
+    merger.hookstub(0x080974de,   #0x0809661e,    #USB manufacturer string handler function.
                     sapplet.getadr("getmfgstr"));
-    merger.hookstub(   #0x080226d2, #startup_botline
+    merger.hookstub(0x080229ae,  #0x080226d2, #startup_botline
                     sapplet.getadr("demo"));
-    merger.hookstub(   #0x08016a96,
+    merger.hookstub(0x08016b7e,  #0x08016a96,
                     sapplet.getadr("loadfirmwareversion_hook"));
-    merger.hookbl(     #0x0808eb66, #Call to usb_dfu_upload().
+    merger.hookbl(  0x0808f9a6,   #0x0808eb66, #Call to usb_dfu_upload().
                   sapplet.getadr("usb_upld_hook"));
                                                                                                                 
                                                                                                                     
