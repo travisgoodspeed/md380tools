fd88c0f9c451b5b506483a80755f033bfb7ad1e4
Simon IJskes <simon@ijskes.org>
2016-09-07 23:16:31 +0200

    radio_config struct started.

--- applet/src/radio_config.h
+++ applet/src/radio_config.h
@@ -16,6 +16,7 @@ struct radio_config {
     uint8_t unk1[4];   // [0]
     uint32_t dmrid;    // [4]
     uint8_t unk2[13];   // [8]
+    // [11] VOX
     uint8_t backlight_time ; // [21] // times 5 seconds.
     uint8_t off22 ; // [22]
     uint8_t mode_ch_mr ; // [23] 255 = CH / 0 = MR
