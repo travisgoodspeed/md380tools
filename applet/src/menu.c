32ae74315027aaf413a628d6ba798115fa733885
Simon IJskes <simon@ijskes.org>
2016-09-10 10:31:27 +0200

    greenkey mapping

--- applet/src/menu.c
+++ applet/src/menu.c
@@ -974,9 +974,19 @@ void create_menu_entry_edit_dmr_id_screen(void)
 
 
 
-void create_menu_entry_addl_functions_screen(void) {
+void create_menu_entry_addl_functions_screen(void) 
+{
   struct MENU *menu_mem;
   int i;
+  
+#if 1   
+    register uint32_t *sp asm("sp");   
+    for(int i=0;i<20;i++) {
+        printf( "%d : 0x%x\n", i, sp[i] );        
+    }
+    //printf( "f menucall.%s 0 0x%x\n", lbl2, (sp[15] - 1 - 4) );
+#endif    
+  
 
 #ifdef DEBUG
   printf("create_menu_entry_addl_functions_screen %d\n",md380_menu_depth);
