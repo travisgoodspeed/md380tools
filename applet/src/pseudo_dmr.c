6657d11922f897d3ed19c5f5b29ed3565df96b15
Simon IJskes <simon@ijskes.org>
2016-09-28 10:58:29 +0200

    add dumping of data pdu

--- applet/src/pseudo_dmr.c
+++ applet/src/pseudo_dmr.c
@@ -57,6 +57,7 @@ void F_414()
             // dmr call end
             goto 0x080408d0 ;
         case 3 :
+            // CBSK
             // post 7 to radio_event_mbox
             goto 0x080408e6 ;
         case 4 :
@@ -67,15 +68,18 @@ void F_414()
             return ;
         case 6 :
             // data_hdr
-            // sms arrive
+            // (sms arrive)
+            0x08040028(130,0,buf,12);
             goto 0x08040916 ;
         case 7 :
             // data_rate_1_2
-            // dmr data
+            // (dmr data)
+            0x08040028(130,0,buf,12);
             goto 0x0804092c ;
         case 8 :
             // data_rate_3_4
-            // dmr data
+            // {dmr data)
+            0x08040028(130,0,buf,18);
             goto 0x08040944 ;
         case 9 :
             // idle
@@ -84,7 +88,8 @@ void F_414()
             goto 0x0804095c ;
         case 10 :
             // data_rate_1
-            // dmr data
+            // (dmr data)
+            0x08040028(130,0,buf,24);
             goto 0x0804098a ;
         default:
             return ;
