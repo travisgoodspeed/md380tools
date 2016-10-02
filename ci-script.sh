eafeb10012d0c0a833803854c778cb434b42b0fc
Simon IJskes <simon@ijskes.org>
2016-10-02 17:11:45 +0200

    use integrated makefile

--- ci-script.sh
+++ ci-script.sh
@@ -2,12 +2,14 @@ set -e errexit	#otherwise bash continues after error!
 make clean
 make all # d13.020
 make clean
-cd applet
-make -f Makefile.d02.032
-cd ..
+#cd applet
+#make -f Makefile.d02.032
+#cd ..
+make image_D02
 make clean
-cd applet
-make -f Makefile.s13.020
-cd ..
+#cd applet
+#make -f Makefile.s13.020
+#cd ..
+make image_S13
 cd db
 make all
