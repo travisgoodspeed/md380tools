3dc3098e2ba2e0f8e743bf5abd923cd211016ed3
Simon IJskes <simon@ijskes.org>
2016-10-02 16:36:06 +0200

    implemented separate call state tracking

---
+++ applet/src/radiostate.h
@@ -0,0 +1,28 @@
+/*
+ *  radiostate.h
+ * 
+ */
+
+#ifndef RADIOSTATE_H
+#define RADIOSTATE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int rst_voice_active ;    
+extern int rst_mycall ;
+    
+void rst_voice_lc_header(int src, int dst);
+
+void rst_term_with_lc( int src, int dst );
+
+void rst_signal_other_call();
+void rst_signal_my_call();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* RADIOSTATE_H */
+
