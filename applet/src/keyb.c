7d74e32be823c86d48e34b5c2766559de47a918e
Simon IJskes <simon@ijskes.org>
2016-10-05 14:24:22 +0200

    party tricks

--- applet/src/keyb.c
+++ applet/src/keyb.c
@@ -7,6 +7,7 @@
 
 #include "debug.h"
 #include "netmon.h"
+#include "mbox.h"
 
 #include <stdint.h>
 
@@ -16,6 +17,34 @@ uint8_t *keycode_p = 0x2001e890 ;
 inline int get_main_mode()
 {
     return md380_f_4225_operatingmode & 0x7F ;
+}
+
+int beep_event_probe = 0 ;
+
+void handle_hotkey( int keycode )
+{
+    switch( keycode ) {
+        case 7 :
+            global_addl_config.console = 0 ;
+            break ;
+        case 8 :
+            global_addl_config.console = 1 ;
+            break ;
+        case 9 :
+            global_addl_config.console = 2 ;
+            break ;
+        case 11 :
+            beep_event_probe++ ;
+            mb_send_beep(beep_event_probe);
+            break ;
+        case 12 :
+            beep_event_probe-- ;
+            mb_send_beep(beep_event_probe);
+            break ;
+        case 15 :
+            global_addl_config.console = 3 ;
+            break ;
+    }    
 }
 
 void trace_keyb()
@@ -36,25 +65,31 @@ void trace_keyb()
     }
     
     int keycode = *keycode_p ;
-    
     switch( keycode ) {
         case 7 :
-            global_addl_config.console = 0 ;
-            break ;
         case 8 :
-            global_addl_config.console = 1 ;
-            break ;
         case 9 :
-            global_addl_config.console = 2 ;
-            break ;
+        case 11 :
+        case 12 :
         case 15 :
-            global_addl_config.console = 3 ;
             break ;
         default:
-            // early exit
+            // don't process
             return ;
     }
+
+    // detect transition
+    static int oldstate = -1 ;
     
+    int key_is_pressed = keypressed != 0 ;
+    
+    if( oldstate != key_is_pressed && key_is_pressed ) {
+        handle_hotkey(keycode);
+    }
+    
+    oldstate = key_is_pressed ;
+    
+    // eat key event.
     *keypressed_p = 0 ;
 }
 
