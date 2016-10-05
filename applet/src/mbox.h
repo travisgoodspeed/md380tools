654c7d7a5b155f9874fb3ea85efc258bdd1810ee
Simon IJskes <simon@ijskes.org>
2016-10-05 14:23:51 +0200

    fw indep mbox

---
+++ applet/src/mbox.h
@@ -0,0 +1,24 @@
+/*
+ *  mbox.h
+ * 
+ * safe usage across fw versions.
+ * 
+ */
+
+#ifndef MBOX_H
+#define MBOX_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void mb_send_beep( int beep );
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MBOX_H */
+
