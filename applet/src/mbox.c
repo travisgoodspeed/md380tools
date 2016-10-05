7a9a40f13aa7ed43a4336e6fde6d745962d1f608
Simon IJskes <simon@ijskes.org>
2016-10-05 14:37:20 +0200

    multifw fix

--- applet/src/mbox.c
+++ applet/src/mbox.c
@@ -7,18 +7,8 @@
 
 #include "md380.h"
 
-#if defined(FW_D13_020)
 extern OS_EVENT *mbox_radio ; //= (OS_EVENT *)0x20017468 ;
 extern OS_EVENT *mbox_beep  ; //= (OS_EVENT *)0x20017390 ;
-#elif defined(FW_D02_032)
-//OS_EVENT *mbox_radio = (OS_EVENT *)0x20017468 ; // wrong
-OS_EVENT *mbox_beep = (OS_EVENT *)0x20015f0c ;
-#warning please consider finding mbox pointers for this firmware version                
-#else
-//OS_EVENT *mbox_radio = (OS_EVENT *)0x20017468 ; // wrong
-//OS_EVENT *mbox_beep = (OS_EVENT *)0x20017390 ; // wrong
-#warning please consider finding mbox pointers for this firmware version                
-#endif
 
 #if defined(FW_D13_020)
 
