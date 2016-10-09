03a392468620da666b3138b6e3546f86927591a7
Simon IJskes <simon@ijskes.org>
2016-10-09 23:44:55 +0200

    fixed config load

--- applet/src/addl_config.c
+++ applet/src/addl_config.c
@@ -18,105 +18,6 @@
 
 addl_config_t global_addl_config;
 
-uint8_t spiflash_read_uint8( int offset )
-{
-    char buf[1];
-    md380_spiflash_read(buf, spi_flash_addl_config_start + offset, 1);
-    return buf[0] - '0' ;
-}
-
-void spiflash_write_uint8( int offset, uint8_t val )
-{
-    char buf[2];
-    buf[0] = '0' + val ;
-    buf[1] = 0 ;
-    spiflash_write_with_type_check(buf, spi_flash_addl_config_start + offset, 1);
-}
-
-uint8_t spiflash_read_uint8_ranged( int offset, uint8_t cnt )
-{
-    uint8_t r = spiflash_read_uint8( offset );
-    if( r >= cnt ) {
-        // out of range, reset to default = 0
-        r = 0 ;
-    }
-    return r ;
-}
-
-inline void spiflash_write_datef()
-{
-    spiflash_write_uint8( offset_datef, global_addl_config.datef );    
-}
-
-inline void spiflash_read_datef()
-{
-    global_addl_config.datef = spiflash_read_uint8_ranged( offset_datef, 6 );    
-}
-
-inline void spiflash_write_console()
-{
-    spiflash_write_uint8( offset_console, global_addl_config.console );    
-}
-
-inline void spiflash_read_console()
-{
-#if defined(FW_D13_020)    
-    global_addl_config.console = spiflash_read_uint8_ranged( offset_console, 4 );    
-#else
-    global_addl_config.console = 0 ;
-#endif    
-}
-
-inline void spiflash_write_promtg()
-{
-    spiflash_write_uint8( offset_promtg, global_addl_config.promtg );    
-}
-
-inline void spiflash_read_promtg()
-{
-    global_addl_config.promtg = spiflash_read_uint8_ranged( offset_promtg, 2 );    
-}
-
-inline void spiflash_write_micbargraph()
-{
-    spiflash_write_uint8( offset_micbargraph, global_addl_config.micbargraph );    
-}
-
-inline void spiflash_write_rbeep()
-{
-    spiflash_write_uint8( offset_rbeep, global_addl_config.rbeep );    
-}
-
-inline void spiflash_read_rbeep()
-{
-    global_addl_config.rbeep = spiflash_read_uint8_ranged( offset_rbeep, 2 );    
-}
-
-inline void spiflash_write_debug()
-{
-    spiflash_write_uint8( offset_debug, global_addl_config.debug );    
-}
-
-inline void spiflash_read_debug()
-{
-    global_addl_config.debug = spiflash_read_uint8_ranged( offset_debug, 2 );    
-}
-
-inline void spiflash_read_micbargraph()
-{
-    global_addl_config.micbargraph = spiflash_read_uint8_ranged( offset_micbargraph, 2 );    
-}
-
-inline void spiflash_write_userscsv()
-{
-    spiflash_write_uint8( offset_userscsv, global_addl_config.userscsv );    
-}
-
-inline void spiflash_read_userscsv()
-{
-    global_addl_config.userscsv = spiflash_read_uint8_ranged( offset_userscsv, 2 );    
-}
-
 void cfg_read_struct( addl_config_t *cfg )
 {
     md380_spiflash_read(cfg, spi_flash_addl_config_start, sizeof(addl_config_t));
@@ -125,30 +26,6 @@ void cfg_read_struct( addl_config_t *cfg )
 void cfg_write_struct( addl_config_t *cfg )
 {
     spiflash_write_with_type_check(cfg, spi_flash_addl_config_start, sizeof(addl_config_t));
-}
-
-void read_compat()
-{
-    spiflash_read_rbeep();
-    spiflash_read_datef();
-    spiflash_read_userscsv();
-    spiflash_read_debug();
-    spiflash_read_promtg();
-    spiflash_read_micbargraph();
-    spiflash_read_console();    
-}
-
-void write_compat()
-{
-    // compat
-    spiflash_write_promtg();
-    spiflash_write_micbargraph();
-    spiflash_write_micbargraph();
-    spiflash_write_rbeep();
-    spiflash_write_datef();
-    spiflash_write_userscsv();
-    spiflash_write_debug();
-    spiflash_write_console();    
 }
 
 uint8_t calc_crc( void *buf, int size)
@@ -161,6 +38,8 @@ uint8_t calc_crc( void *buf, int size)
     return crc ;
 }
 
+#define R( ii, max ) { if( ii < 0 || ii > max ) { ii = 0 ; } }
+
 void cfg_load()
 {
     memset( &global_addl_config, 0, sizeof(addl_config_t) );
@@ -168,19 +47,32 @@ void cfg_load()
     addl_config_t tmp ;    
     cfg_read_struct( &tmp );
     
+    // the config in flash is bigger than mine.
+    if( tmp.length > sizeof(addl_config_t) ) {
+        // we cannot crc what we have not read.
+        LOGB("cfg oversized\n");
+        return ;        
+    }
+    
     if( calc_crc(&tmp,tmp.length) != 0 ) {
         // corrupted.
         LOGB("cfg crc fail\n");
         return ;
     }
-    
-    if( tmp.length > sizeof(addl_config_t) ) {
-        LOGB("cfg too big\n");
-        tmp.length = sizeof(addl_config_t);
-    }
-    
+
+    // copy the smaller config into our bigger config.
+    // leaving the rest 0.
     memcpy(&global_addl_config,&tmp,tmp.length);
-        
+    
+    // range limit
+    R(global_addl_config.userscsv,1);
+    R(global_addl_config.micbargraph,1);
+    R(global_addl_config.debug,1);
+    R(global_addl_config.rbeep,1);
+    R(global_addl_config.promtg,1);
+    R(global_addl_config.console,3);
+    R(global_addl_config.datef,5);
+    
     // restore dmrid
     int dmrid = global_addl_config.dmrid ;
     if( dmrid != 0 ) {
@@ -190,6 +82,11 @@ void cfg_load()
     
     // global_addl_config.experimental is intentionally not permanent
     global_addl_config.experimental = 0;
+
+#if defined(FW_D13_020)    
+#else
+    global_addl_config.console = 0 ;
+#endif    
 }
 
 void cfg_save()
@@ -204,13 +101,12 @@ void cfg_save()
 
 void init_global_addl_config_hook(void)
 {
-    cfg_load();
-
     LOGB("booting\n");
     
-#ifdef CONFIG_MENU
+    cfg_load();
+
+//#ifdef CONFIG_MENU
     md380_create_main_meny_entry();
-#endif
-    
+//#endif    
 }
 
