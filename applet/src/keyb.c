1d9ed4b93ba7cdf5c24e6ae83a5a201b5b3bbd64
Simon IJskes <simon@ijskes.org>
2016-10-11 19:04:47 +0200

    keyboard probing

--- applet/src/keyb.c
+++ applet/src/keyb.c
@@ -29,6 +29,8 @@ int beep_event_probe = 0 ;
 
 void handle_hotkey( int keycode )
 {
+    PRINT("handle hotkey: %d\n", keycode );
+    
     switch( keycode ) {
         case 5 :
             syslog_clear();
@@ -42,9 +44,9 @@ void handle_hotkey( int keycode )
             break ;
         case 7 :
             global_addl_config.console = 0 ;
-            gui_opmode2 = OPM2_IDLE ;
+            // cause transient.
+            gui_opmode2 = OPM2_MENU ;
             md380_f_4225_operatingmode = SCR_MODE_IDLE | 0x80 ;
-//            md380_f_4225_operatingmode = md380_f_4225_operatingmode | 0x80 ;
             break ;
         case 8 :
             global_addl_config.console = 1 ;
@@ -121,46 +123,94 @@ void intercept_keyb()
     *keypressed_p = 0 ;
 }
 
-void trace_keyb()
+void trace_keyb(int sw)
 {
     static uint8_t old_kp = -1 ;
     uint8_t kp = *keypressed_p ;
     
-    if( old_kp != kp ) {
-        LOGR("kp: %02x -> %02x (%04x) (%d)\n", old_kp, kp, *kb_row_col_pressed, *kb_keycode );
+    if( old_kp != kp ) {        
+        PRINT("kp: %d %02x -> %02x (%04x) (%d)\n", sw, old_kp, kp, *kb_row_col_pressed, *kb_keycode );
         old_kp = kp ;
     }
 }
 
-extern void kb_handler();
+inline int is_intercept_allowed()
+{
+//    switch( get_main_mode() ) {
+//        case 28 :
+//            return 1 ;
+//        default:
+//            return 0 ;
+//    }
+    switch( gui_opmode2 ) {
+        case OPM2_MENU :
+            return 0 ;
+        default:
+            return 1 ;
+    }
+}
 
-uint32_t kb_handler_count = 0;
+inline int is_intercepted_keycode( int kc )
+{
+    switch( kc ) {
+        case 5 :            
+        case 6 :
+        case 7 :
+        case 8 :
+        case 9 :
+        case 11 :
+        case 12 :
+        case 15 :
+            return 1 ;
+        default:
+            return 0 ;
+    }    
+}
+
+extern void kb_handler();
 
 void kb_handler_hook()
 {
-    kb_handler_count++;
+    trace_keyb(0);
     
-    if( global_addl_config.experimental ) {
-        static uint16_t old = 0 ;
-        uint16_t new = *kb_row_col_pressed ;
-        if( old != new ) {
-            LOGR("rc: %04x -> %04x \n", old, new );
-            old = new ;
-        }
-        *kb_row_col_pressed = 0 ;
-    }
-
 #if defined(FW_D13_020)
     kb_handler();
 #else
 #warning please consider hooking.    
 #endif    
-    if( global_addl_config.experimental ) {
-        //trace_keyb();
-        return;
+    
+    trace_keyb(1);
+
+    if( is_intercept_allowed() ) {
+        int kc = *kb_keycode ;
+        if( is_intercepted_keycode(kc) ) {
+            int kp = *keypressed_p ;
+            
+            if( kp == 2 ) {
+                *keypressed_p = 0 ;
+                handle_hotkey(kc);
+                return ;
+            }
+        }        
     }
-    if( global_addl_config.debug ) {
-        intercept_keyb();
-    }
+    
+//    if( global_addl_config.experimental ) {
+//        static uint16_t old = 0 ;
+//        uint16_t new = *kb_row_col_pressed ;
+//        if( old != new ) {
+//            LOGR("rc: %04x -> %04x \n", old, new );
+//            old = new ;
+//        }
+//        *kb_row_col_pressed = 0 ;
+//    }
+
+//    if( global_addl_config.experimental ) {
+//        trace_keyb();
+//        return;
+//    }
+    
+//    if( global_addl_config.debug ) {
+//        intercept_keyb();
+//    }
 
 }
