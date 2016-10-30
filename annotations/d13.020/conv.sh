ff8aef497277d59b13a9409ac8ccbe3abab0b9f1
Simon IJskes <simon@ijskes.org>
2016-10-30 09:23:37 +0100

    conversion

--- annotations/d13.020/conv.sh
+++ annotations/d13.020/conv.sh
@@ -1,12 +1,25 @@
 
 SYM=sym.tmp
 
+#link_2_ra()
+#{
+#
+#}
+
+sym2ra()
+{
+awk -f sym2ra.awk
+}
+
 awk -f ra2sym.awk flash.r | sort >$SYM
 
 awk -f sym2ra.awk $SYM >r2.tmp
 awk -f sym2lnk.awk $SYM >lnk.tmp
 
 cat ../../applet/src/symbols_d13.020 | sed 's/;/ ; /' | awk -f lnk2sym.awk | sort >sym_d13.tmp
+
+cat sym_d13.tmp | sym2ra >ra.tmp 
+
 awk '{ print $1 OFS $2 }' $SYM >sym2.tmp
 
 #diff <( sort lnk.tmp ) <( sort ../../applet/src/symbols_d13.020) 
