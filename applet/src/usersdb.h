5ab9d1d07b0409ff523ebdd44333a441b5a05249
Simon IJskes <simon@ijskes.org>
2016-11-03 09:28:47 +0100

    reduced interface

--- applet/src/usersdb.h
+++ applet/src/usersdb.h
@@ -1,8 +1,3 @@
-/**
- * lookup the a user given their ID (dmr_search) in the database.
- * The function returns 1 for success and 0 for "not found".
- * outsize refers to the size of the output buffer.
- */
 
 #ifndef _USERSDB_H
 #define _USERSDB_H
@@ -20,10 +15,10 @@ typedef struct {
     char *country ;
 } user_t ;
 
+/**
+ * lookup the a user given their ID (dmr_search) in the database.
+ * The function returns 1 for success and 0 for "not found".
+ */
 int usr_find_by_dmrid( user_t *up, int dmrid );
-
-//extern int find_dmr_user(char *outstr, int dmr_search, const char *data, int outsize);
-
-//extern uint8_t get_dmr_user_field(uint8_t field, char *outstr, int dmr_search, int outsize);
 
 #endif
