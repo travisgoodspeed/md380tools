a200d91fc652028b21e51a6dc54ead04bf5d90b8
Simon IJskes <simon@ijskes.org>
2016-11-29 23:29:55 +0100

    common.mk

--- annotations/common.mk
+++ annotations/common.mk
@@ -3,3 +3,6 @@
 radare: orig.img
 	radare2 -a arm -m 0x0800C000  -b 16 -i ../cpu.r -i flash.r orig.img
 	
+asm.lst: flash.r Makefile disasm.r orig.img
+	-mv asm.lst asm1.lst
+	radare2 -a arm -m 0x0800C000  -b 16 -i disasm.r -q orig.img
