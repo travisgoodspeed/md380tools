209fdea317492f00fdd114ff813aee37b7c93556
Simon IJskes <simon@ijskes.org>
2016-11-29 23:31:54 +0100

    common.mk

--- annotations/common.mk
+++ annotations/common.mk
@@ -2,7 +2,12 @@
 
 radare: orig.img
 	radare2 -a arm -m 0x0800C000  -b 16 -i ../cpu.r -i flash.r orig.img
-	
+
 asm.lst: flash.r Makefile disasm.r orig.img
 	-mv asm.lst asm1.lst
 	radare2 -a arm -m 0x0800C000  -b 16 -i disasm.r -q orig.img
+
+sync:
+	../conv/upd_r2.sh $(SYMBOLS) flash.r >flash_new.tmp
+	../conv/ra2lnk.sh <flash.r >$(SYMBOLS)_new.tmp
+
