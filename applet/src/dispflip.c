46416f46893dfcf61039dc14566350f5affbde78
Alexander Eickhoff <alex@artbanause.de>
2016-12-07 00:15:40 +0100

    add dispinit from s013.032 for now display type

--- applet/src/dispflip.c
+++ applet/src/dispflip.c
@@ -14,18 +14,18 @@ void display_init_hook_1(void) {      // from 0x8033586 @ D003.020
   md380_copy_spiflash_security_bank2_to_ram();
   if ( md380_radio_config_bank2[0x1d] & 1)  // offset 0x1d from
                                             // 0x08033586 @ D003.020
-   Write_Data_2display(0x8);                // MD380
+   md380_Write_Data_2display(0x8);                // MD380
   else
-   Write_Data_2display(0x48);               // MD390
+   md380_Write_Data_2display(0x48);               // MD390
 #endif
 }
 
 void display_init_hook_2(void) {
 #ifdef CONFIG_GRAPHICS
   if ( md380_radio_config_bank2[0x1d] & 1)
-    Write_Data_2display(0x40);
+    md380_Write_Data_2display(0x40);
   else
-    Write_Data_2display(0x4f);
+    md380_Write_Data_2display(0x4f);
 #endif
 }
 
