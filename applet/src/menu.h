04f1ecc2ba48509607dc8b44a0955643d838226c
Wolf-DL4YHF <dl4yhf@freenet.de>
2017-03-13 22:10:33 +0100

    added function prototype to reduce number of warnings
    create_menu_entry_set_tg_screen() was called somewhere without a
    prototype.
    Prototype added in the header file, so other modules can call it
    properly.

--- applet/src/menu.h
+++ applet/src/menu.h
@@ -17,10 +17,15 @@ extern wchar_t  	md380_menu_edit_buf[];
 #if defined(FW_D13_020) || defined(FW_S13_020)
 extern uint8_t      currently_selected_menu_entry;
 
-
 #endif
 /* mn_editbuffer_poi / md380_menu_0x20001114 */
 extern wchar_t *mn_editbuffer_poi;
+
+
+/* Don't call functions without a complete prototype, e.g. from keyb.c . */
+/* See also (more menu functions) : md380.h !                            */
+
+void create_menu_entry_set_tg_screen(void);
 
 
 #ifdef __cplusplus
