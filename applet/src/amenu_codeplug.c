c3349595ea261c28f65028131e09cd2f7ae43c70
Wolf-DL4YHF <dl4yhf@freenet.de>
2017-05-14 23:06:31 +0200

    eliminate a warning - see PR #745

--- applet/src/amenu_codeplug.c
+++ applet/src/amenu_codeplug.c
@@ -48,7 +48,9 @@ BOOL ZoneList_ReadNameByIndex( int index,             // [in] zero-based zone in
 BOOL ZoneList_SetZoneByIndex( int index )  // [in] zero-based zone index
 {
   wchar_t wc16Temp[20];
+#ifdef CODEPLUG_RAM_ADDR_ZONE_NUMBER_STRUCT
   zone_number_t *pZoneStruct;
+#endif
 
   // Summary of 'zone-related' variables and functions, with addresses for D13.020 (RAM, SPI-Flash) :
   // - zone_data_64byte @ 0x2001e218 : Seems to be a 64 byte structure with the 
@@ -84,7 +86,7 @@ BOOL ZoneList_SetZoneByIndex( int index )  // [in] zero-based zone index
 #      endif
       }
      // Imitate some of the code at 0x08013418 in D13.020 ...
-     // This possibly the stuff called when 'confirming' a new zone in Tytera's original menu.
+     // This is possibly the stuff called when 'confirming' a new zone in Tytera's original menu.
      // There seems to be a FIVE-BYTE struct with the ZONE NUMBER at byte-offset #3. 
      // It lives in SPI-flash (at 0x2F000?), and there's a copy in Tytera's part of the RAM:
 #   ifdef CODEPLUG_RAM_ADDR_ZONE_NUMBER_STRUCT /* 0x2001E57C in D13.020, ymmv.. */
