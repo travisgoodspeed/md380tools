135465175467856eb88d352c607ba52cdbbc9766
Wolf-DL4YHF <dl4yhf@freenet.de>
2017-06-11 00:30:45 +0200

    added TFDU_READ_FRAMEBUFFER (USB "command")

--- applet/src/tooldfu.h
+++ applet/src/tooldfu.h
@@ -32,4 +32,5 @@ fetching the raw buffer.
 #define TDFU_PRINT 0x80 //(u8 x, u8 y, wchar_t str[])
 #define TDFU_BOX   0x81 //(u8 x, u8 y, u8 xp, u8 yp)
 
-#define TDFU_READ_FRAMEBUFFER 0x84 // (u8 x1, u8 y1, u8 x2, u8 y2) max tile size = 512 pixels = 1 kByte
+// Framebuffer transfer commands
+#define TDFU_READ_FRAMEBUFFER_24BPP 0x84 // (u8 x1, u8 y1, u8 x2, u8 y2)
